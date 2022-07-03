f8699a826bb18bb330cd89557d72b45e67ad6c09
ruslannagnibeda <ruslannagnibedaa@gmail.com>
2022-07-03 18:43:04 +0300

    Initial commit

---
+++ API.py
@@ -0,0 +1,108 @@
+from datetime import datetime
+from flask import jsonify
+from flask import Flask
+from werkzeug.exceptions import default_exceptions, Aborter
+from flask_restful import Api, Resource, reqparse
+from convert_data import CandleData
+from DB import DB
+import werkzeug.exceptions as ex
+import json
+
+
+class UncorrectTimeframe(ex.HTTPException):
+    code = 203
+    description = 'timeframe should be in :1m, 2m,5m,15m,30m,1h,2h,8h,1d,1w'
+
+
+default_exceptions[203] = UncorrectTimeframe
+abort = Aborter()
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Candle(Resource):
+    def __init__(self):
+        self.db = DB()
+
+    def get(self, project_id: int, timeframe: int, start_time: str, limit: int):
+        start_time = datetime.strptime(start_time, '%d.%m.%Y')
+        start_time = int(round(start_time.timestamp() * 1000))
+
+        full_data = self.db.get_parsed_values(project_id, start_time)
+        price_and_time = [i[2:4] for i in full_data]
+        candle = CandleData(price_and_time)
+
+        if timeframe == '1m':
+            candle_data = candle.make_candles(1)
+        elif timeframe == '2m':
+            candle_data = candle.make_candles(2)
+        elif timeframe == '5m':
+            candle_data = candle.make_candles(5)
+        elif timeframe == '15m':
+            candle_data = candle.make_candles(15)
+        elif timeframe == '30m':
+            candle_data = candle.make_candles(30)
+        elif timeframe == '1h':
+            candle_data = candle.make_candles(60)
+        elif timeframe == '2h':
+            candle_data = candle.make_candles(120)
+        elif timeframe == '4h':
+            candle_data = candle.make_candles(240)
+        elif timeframe == '8h':
+            candle_data = candle.make_candles(480)
+        elif timeframe == '1d':
+            candle_data = candle.make_candles(1440)
+        elif timeframe == '1w':
+            candle_data = candle.make_candles(10080)
+        else:
+            return abort(203)
+
+        return candle_data[0:limit]
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("project_id", nullable=False, type=int, trim=True)
+        parser.add_argument("price", nullable=False, type=float, trim=True)
+        parser.add_argument("timestamp", nullable=False, type=int, trim=True)
+        parser.add_argument("chain", nullable=False, type=str, trim=True)
+        params = parser.parse_args()
+
+        self.db.insert_parsed_values(params["project_id"], params["price"], params["timestamp"], params["chain"])
+
+        return params
+
+
+class Projects(Resource):
+    def __init__(self):
+        self.db = DB()
+
+    def get(self):
+        projects = self.db.get_projects()
+
+        res_dict = {}
+        for project in projects:
+            res_dict[project[0]] = {'section': project[1]}, \
+                                   {'filters': project[2]},\
+                                   {'chain': self.db.get_chain_value(project[0])},\
+                                   {'create_time': [project[3], datetime.fromtimestamp(project[3])]}
+        js = jsonify(res_dict)
+        return js
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("section", nullable=False, type=str, trim=True)
+        parser.add_argument("filters", nullable=False, type=str, trim=True)
+        params = parser.parse_args()
+        self.db.insert_project(params["section"], params["filters"])
+
+        return params
+
+
+api.add_resource(Candle, "/candle-data/<int:project_id>/<string:timeframe>/<string:start_time>/<int:limit>",
+                 '/candle-data')
+
+api.add_resource(Projects, '/projects', '/projects')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ DB.py
@@ -0,0 +1,72 @@
+import mysql.connector
+import time
+import yaml
+from yaml import CLoader as Loader
+with open('config.yaml', 'r') as f:
+    config = yaml.load(f, Loader=Loader)['DB']
+
+
+class DB():
+    def __init__(self):
+        self.cnx = mysql.connector.connect(**config)
+        self.cursor = self.cnx.cursor(prepared=True)
+
+    def get_parsed_values(self, project_id: int, start_time: int) -> list:
+        self.cursor.execute(
+            'SELECT projects.section, projects.filters, price, timestamp,chain FROM floor_price INNER JOIN projects ON floor_price.project_id=projects.id WHERE project_id=(?) AND timestamp>=(?) ORDER BY timestamp ASC',
+            (project_id, start_time))
+        data = self.cursor.fetchall()
+
+        return data
+
+    def get_chain_value(self, project_id: int) -> str:
+        self.cursor.execute('SELECT chain FROM floor_price WHERE project_id=(?) LIMIT 1', (project_id,))
+        data = self.cursor.fetchall()
+
+        try:
+            return data[0][0]
+        except IndexError:
+            return ''
+
+    def get_project_id(self, section: str, filters: str) -> int:
+        data= self.get_projects()
+        for i in data:
+            if section == i[1] and filters == i[2]:
+                return int(i[0])
+
+        project_id = self.insert_project(section, filters)
+
+        return project_id
+
+    def get_projects(self) -> list:
+        self.cursor.execute('SELECT * FROM projects')
+        data = self.cursor.fetchall()
+
+        return data
+
+    def __get_max_value(self) -> int:
+        self.cursor.execute('SELECT max(id) FROM projects')
+        data = self.cursor.fetchall()
+
+        return int(data[0][0])
+
+    def insert_parsed_values(self, project_id: int, price: float, timestamp: int, chain: str) -> None:
+        self.cursor.execute('INSERT INTO floor_price(project_id,price,timestamp,chain) VALUES (?,?,?,?)',
+                            (project_id, price, timestamp, chain))
+        self.cnx.commit()
+
+    def insert_project(self, section: str, filters: str) -> int:
+        create_time = int(time.time())
+        self.cursor.execute('INSERT INTO projects(section, filters, create_time) VALUES (?,?,?)', (section, filters, create_time))
+        self.cnx.commit()
+
+        return self.__get_max_value()
+
+if __name__ == '__main__':
+    db = DB()
+    time_1=time.time()
+    values=db.get_projects()
+    print('time= ', time.time()-time_1)
+    print(values)
+
+
